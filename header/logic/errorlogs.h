24a82626b7c5d1eec000e64c885bc414bd1ab37f
DyPacheco <243149497+Dioggo2007@users.noreply.github.com>
2026-05-12 16:59:51 +0100

    Exemplos de logs de erros

--- header/logic/errorlogs.h
+++ header/logic/errorlogs.h
@@ -8,4 +8,11 @@
 //Criar um ficheiro ou adicionar ao ficheiro uma linha de erro ou aviso(ERROR/WARNING)
 void createLog(char *nivel, char *mensagem);
 
+/*
+ * Exemplos:
+ * createLog("ERROR", "Nenhum produto encontrado no ficheiro txt");
+ * createLog("ERROR", "Memória insuficiente para alocar produtos!");
+ * createLog("WARNING", "Não foi possivel abrir o ficheiro de configurações!");
+ */
+
 #endif //GEST_O_DE_CAIXAS_SUPERMERCADO_PROJETO_ERRORLOGS_H
